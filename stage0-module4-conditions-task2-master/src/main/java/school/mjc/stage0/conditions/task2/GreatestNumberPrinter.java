e89a681f5db019b0dc8ef1e048111bce221d58f2
bulldog3000 <128056980+bulldog3000@users.noreply.github.com>
2023-10-12 12:51:32 +0300

    Commited

---
+++ stage0-module4-conditions-task2-master/src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -0,0 +1,15 @@
+package school.mjc.stage0.conditions.task2;
+
+public class GreatestNumberPrinter {
+    public void printGreatest(int first, int second) {
+        if (first > second){
+            System.out.println(first);
+        }
+        if (second > first) {
+            System.out.println(second);
+        }
+        if (second == first){
+            System.out.println(first);
+        }
+    }
+}
